87d0f33acd09991fe8ae6d2c9cc8d00ae3b4b725
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2024-12-04 22:58:07 -0800

    clean up RoleConverter somewhat
    I probably could get away with referencing RolePlus in the ConvertBaseToBTOS but i don't want to trust the compiler to convert it to constants

--- src/RoleConverter.cs
+++ src/RoleConverter.cs
@@ -1,217 +1,219 @@
-﻿public class RoleConverter
+﻿using Server.Shared.State;
+using BetterTOS2;
+public class RoleConverter
 {
-    public static byte ConvertBTOSToBase(byte role)
+    public static byte ConvertBTOSToBase(byte role) => role switch
     {
-        switch (role)
-        {
-            //marshal
-            case 56:
-                return 55;
+        //I can't reference RolePlus here because this code can be run in a non-btos2 state
 
-            //oracle -> town protective
-            case 61:
-                return 102;
+        // these don't exist in base so they need to be given a bucket instead [except warlock->sc]
+        // oracle -> tp
+        61 => (byte)Role.TOWN_PROTECTIVE,
 
-            //warlock -> soul collector
-            case 62:
-                return 50;
+        // warlock -> sc
+        62 => (byte)Role.SOULCOLLECTOR,
 
-            //common neutral -> neutral evil
-            case 121:
-            //neutral pariah -> neutral evil
-            case 119:
-            //starspawn -> neutral evil
-            case 60:
-            //auditor -> neutral evil
-            case 58:
-            //judge -> neutral evil
-            case 57:
-            //inquisitor -> neutral evil
-            case 59:
-                return 113;
+        // common neutral -> neutral evil
+        121 => (byte)Role.NEUTRAL_EVIL,
 
-            //neutral special -> random neutral
-            case 120:
-            //jackal -> random neutral
-            case 55:
-                return 111;
+        // neutral pariah -> neutral evil
+        119 => (byte)Role.NEUTRAL_EVIL,
 
-            //banshee -> coven deception
-            case 54:
-                return 109;
+        // starspawn -> neutral evil
+        60 => (byte)Role.NEUTRAL_EVIL,
 
-            //any -> any
-            case 101:
-            case 100:
-                return 115;
+        // auditor -> neutral evil
+        58 => (byte)Role.NEUTRAL_EVIL,
 
-            //random town
-            case 102:
-                return 100;
-            //town investigative
-            case 104:
-                return 101;
-            //town protective
-            case 106:
-                return 102;
-            //town killing
-            case 107:
-                return 103;
-            //town support
-            case 108:
-                return 104;
-            //town power matches up
-            //random coven
-            case 109:
-                return 106;
-            //coven killing
-            case 112:
-                return 107;
-            //coven utility
-            case 114:
-                return 108;
-            //coven deception
-            case 111:
-                return 109;
-            //coven power
-            case 113:
-                return 110;
-            //random neutral
-            case 116:
-                return 111;
-            //neutral killing
-            case 118:
-                return 112;
-            //neutral evil
-            case 117:
-                return 113;
-            //neutral apoc
-            case 115:
-                return 114;
-            //common town
-            case 103:
-                return 116;
-            //common coven
-            case 110:
-                return 117;
+        // judge -> neutral evil
+        57 => (byte)Role.NEUTRAL_EVIL,
 
-            //apoc town traitor -> void
-            case 211:
-            //necro passing -> void
-            case 212:
-            //teams -> void
-            case 213:
-            //anon players -> void
-            case 214:
-            //walking dead -> void
-            case 215:
-            //egotist townie -> void
-            case 216:
-            //sneaking spirits -> void
-            case 217:
-            //secret objectives -> void
-            case 218:
-            //no last wills -> void
-            case 219:
-            //immovable -> void
-            case 220:
-            //compliant killers -> void
-            case 221:
-            //pandoras box -> void
-            case 222:
-            //ballot voting -> void
-            case 223:
-            //individuality -> void
-            case 224:
-            //snitch -> void
-            case 225:
-            //coven vip -> void
-            case 226:
-            //secret whispers -> void
-            case 227:
-                return 0;
-            default:
-                return role;
-        }
-    }
+        // inquisitor -> neutral evil
+        59 => (byte)Role.NEUTRAL_EVIL,
+        
+        // neutral special -> random neutral
+        120 => (byte)Role.RANDOM_NEUTRAL,
 
-    public static byte ConvertBaseToBTOS(byte role)
+        // jackal -> random neutral
+        55 => (byte)Role.RANDOM_NEUTRAL,
+
+        // banshee -> coven deception
+        54 => (byte)Role.COVEN_DECEPTION,
+
+        // now the stuff that actually exists and just needs to be converted
+
+        //marshal
+        56 => (byte)Role.MARSHAL,
+
+        // any/true any -> any
+        101 => (byte)Role.ANY,
+        100 => (byte)Role.ANY,
+
+        // random town
+        102 => (byte)Role.RANDOM_TOWN,
+
+        // town investigative
+        104 => (byte)Role.TOWN_INVESTIGATIVE,
+
+        // town protective
+        106 => (byte)Role.TOWN_PROTECTIVE,
+
+        // town killing
+        107 => (byte)Role.TOWN_KILLING,
+
+        // town support
+        108 => (byte)Role.TOWN_SUPPORT,
+
+        // town power matches up
+
+        // random coven
+        109 => (byte)Role.RANDOM_COVEN,
+
+        // coven killing
+        112 => (byte)Role.COVEN_KILLING,
+
+        // coven utility
+        114 => (byte)Role.COVEN_UTILITY,
+
+        // coven deception
+        111 => (byte)Role.COVEN_DECEPTION,
+
+        // coven power
+        113 => (byte)Role.COVEN_POWER,
+
+        // random neutral
+        116 => (byte)Role.RANDOM_NEUTRAL,
+
+        // neutral killing
+        118 => (byte)Role.NEUTRAL_KILLING,
+
+        // neutral evil
+        117 => (byte)Role.NEUTRAL_EVIL,
+
+        // neutral apoc
+        115 => (byte)Role.NEUTRAL_APOCALYPSE,
+
+        // common town
+        103 => (byte)Role.COMMON_TOWN,
+
+        // common coven
+        110 => (byte)Role.COMMON_COVEN,
+
+        //apoc town traitor -> void
+        211 => 0,
+        //necro passing -> void
+        212 => 0,
+        //teams -> void
+        213 => 0,
+        //anon players -> void
+        214 => 0,
+        //walking dead -> void
+        215 => 0,
+        //egotist townie -> void
+        216 => 0,
+        //sneaking spirits -> void
+        217 => 0,
+        //secret objectives -> void
+        218 => 0,
+        //no last wills -> void
+        219 => 0,
+        //immovable -> void
+        220 => 0,
+        //compliant killers -> void
+        221 => 0,
+        //pandoras box -> void
+        222 => 0,
+        //ballot voting -> void
+        223 => 0,
+        //individuality -> void
+        224 => 0,
+        //snitch -> void
+        225 => 0,
+        //coven vip -> void
+        226 => 0,
+        //secret whispers -> void
+        227 => 0,
+        
+        // default case, just return the same role id
+        _ => role,
+    };
+
+    public static byte ConvertBaseToBTOS(byte role) => role switch
     {
-        switch (role)
-        {
-            //socialite -> town support
-            case 54:
-                return 108;
-            //marshal
-            case 55:
-                return 56;
-            //any
-            //maybe change to True Any bc pirate?
-            //cursed soul also doesnt show up here tho
-            case 115:
-                return 101; 
-            //random town
-            case 100:
-                return 102;
-            //town investigative
-            case 101:
-                return 104;
-            //town protective
-            case 102:
-                return 106;
-            //town killing
-            case 103:
-                return 107;
-            //town support
-            case 104:
-                return 108;
-            //town power matches up
-            //random coven
-            case 106:
-                return 109;
-            //coven killing
-            case 107:
-                return 112;
-            //coven utility
-            case 108:
-                return 114;
-            //coven deception
-            case 109:
-                return 111;
-            //coven power
-            case 110:
-                return 113;
-            //random neutral
-            case 111:
-                return 116;
-            //neutral killing
-            case 112:
-                return 118;
-            //neutral evil
-            case 113:
-                return 117;
-            //neutral apoc
-            case 114:
-                return 115;
-            //common town
-            case 116:
-                return 103;
-            //common coven
-            case 117:
-                return 110;
+        //I CAN reference roleplus here as this code is only ever run in a btos2 state :D
+
+        //socialite -> town support
+        (byte)Role.SOCIALITE => (byte)RolePlus.TOWN_SUPPORT,
+
+        //marshal
+        (byte)Role.MARSHAL => (byte)RolePlus.MARSHAL,
+
+        //any
+        //maybe change to True Any bc pirate?
+        //cursed soul also doesnt show up here tho
+        (byte)Role.ANY => (byte)RolePlus.ANY,
+
+        //random town
+        (byte)Role.RANDOM_TOWN => (byte)RolePlus.RANDOM_TOWN,
+
+        //town investigative
+        (byte)Role.TOWN_INVESTIGATIVE => (byte)RolePlus.TOWN_INVESTIGATIVE,
+
+        //town protective
+        (byte)Role.TOWN_PROTECTIVE => (byte)RolePlus.TOWN_PROTECTIVE,
+
+        //town killing
+        (byte)Role.TOWN_KILLING => (byte)RolePlus.TOWN_KILLING,
+
+        //town support
+        (byte)Role.TOWN_SUPPORT => (byte)RolePlus.TOWN_SUPPORT,
+
+        //town power matches
+    
+        //random coven
+        (byte)Role.RANDOM_COVEN => (byte)RolePlus.RANDOM_COVEN,
+
+        //coven killing
+        (byte)Role.COVEN_KILLING => (byte)RolePlus.COVEN_KILLING,
+
+        //coven utility
+        (byte)Role.COVEN_UTILITY => (byte)RolePlus.COVEN_UTILITY,
+        
+        //coven deception
+        (byte)Role.COVEN_DECEPTION => (byte)RolePlus.COVEN_DECEPTION,
+
+        //coven power
+        (byte)Role.COVEN_POWER => (byte)RolePlus.COVEN_POWER,
+        //random neutral
+        (byte)Role.RANDOM_NEUTRAL => (byte)RolePlus.RANDOM_NEUTRAL,
+
+        //neutral killing
+        (byte)Role.NEUTRAL_KILLING => (byte)RolePlus.NEUTRAL_KILLING,
+
+        //neutral evil
+        (byte)Role.NEUTRAL_EVIL => (byte)RolePlus.NEUTRAL_EVIL,
+
+        //neutral apoc
+        (byte)Role.NEUTRAL_APOCALYPSE => (byte)RolePlus.RANDOM_APOCALYPSE,
+
+        //common town
+        (byte)Role.COMMON_TOWN => (byte)RolePlus.REGULAR_TOWN,
+
+        //common coven
+        (byte)Role.COMMON_COVEN => (byte)RolePlus.REGULAR_COVEN,
                 
-            //vip matches
-            //town traitor matches
-            //ghost town matches
-            //perfect town matches
-            //slow mode matches
-            //fast mode matches
-            //anon voting matches
-            //secret killers matches
-            //hidden roles matches
-            //one trial per day matches
-            default:
-                return role;
-        }
-    }
+        //vip matches
+        //town traitor matches
+        //ghost town matches
+        //perfect town matches
+        //slow mode matches
+        //fast mode matches
+        //anon voting matches
+        //secret killers matches
+        //hidden roles matches
+        //one trial per day matches
+
+        // default case, just return the same role id
+        _ => role,
+  };
 }
